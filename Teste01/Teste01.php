5030bce64d3db3c286727bdd902bb5ca7bae0afb
cursosenac4 <131500719+Vitor-Desenvolvedor@users.noreply.github.com>
2023-08-07 21:26:28 -0300

    Update Teste01.php

--- Teste01/Teste01.php
+++ Teste01/Teste01.php
@@ -15,7 +15,7 @@ echo $Saudacao;
 echo '<br><br>';
 
 //(==) Igual
-$valor1=10;
+/*$valor1=10;
 $valor2="10";
 if ($valor1 == $valor2){
 
@@ -38,6 +38,31 @@ if ($valor1 === $valor2){
 
     echo "Os valores são diferentes";
 
-}
+}*/
+
+// Teste com input
+$valor1 = isset($_POST['valor1']) ? $_POST['valor1'] : 10;
+$valor2 = isset($_POST['valor2']) ? $_POST['valor2'] : "10";
+
+$resultado = ($valor1 == $valor2) ? "Os valores são iguais" : "Os valores são diferentes";
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Comparação de Valores</title>
+</head>
+<body>
+
+<form method="post" action="">
+    Valor 1: <input type="text" name="valor1" value="<?php echo $valor1; ?>"><br>
+    Valor 2: <input type="text" name="valor2" value="<?php echo $valor2; ?>"><br>
+    <input type="submit" value="Comparar">
+</form>
+
+<p><?php echo $resultado; ?></p>
+
+</body>
+</html>
+
