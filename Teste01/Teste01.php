6dad458d464ea9331a5eeb655e798ee3fefd49f2
cursosenac4 <131500719+Vitor-Desenvolvedor@users.noreply.github.com>
2023-08-07 21:10:11 -0300

    Criado

--- Teste01/Teste01.php
+++ Teste01/Teste01.php
@@ -1,8 +1,28 @@
 <?php 
 
+// Nome do autor
 $nome = 'Vitor Carvalho'; 
+echo $nome;
 
-echo nome;
+// Concatenar strings
 
+echo '<br><br>';
+
+$nome = "Vitor";
+$Saudacao = "Olá, ".$nome."! Bem vindo ao nosso site";
+echo $Saudacao;
+
+echo '<br><br>';
+//(===) Identico
+$valor1=10;
+$valor2="10";
+if ($valor1 === $valor2){
+
+    echo "Os valores são iguais";
+} else {
+
+    echo "Os valores são diferentes";
+
+}
 
 ?>
